73a715994b8408701b2b3370b7b79167f0cf116c
Rasmuhamed <a.sleszynski@gmail.com>
2015-11-11 15:20:09 +0100

    Implemented HttpURLConnection

--- app/src/main/java/com/example/julian/locationservice/DataUploader.java
+++ app/src/main/java/com/example/julian/locationservice/DataUploader.java
@@ -7,6 +7,15 @@ import com.meedamian.info.BasicData;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DataUploader {
 
     private Context c;
@@ -23,6 +32,10 @@ public class DataUploader {
     public void upload() {
         String key = BasicData.getPrivateId(c);
 
+        HttpURLConnection urlConnection;
+        URL url;
+        String http = "https://basic-data.parseapp.com/update";
+
         JSONObject jo = new JSONObject();
         try {
             jo.put("key", key);
@@ -30,25 +43,44 @@ public class DataUploader {
             if (phone != null)
                 jo.put("phone", phone);
 
-            else if(vanity != null)
+            else if (vanity != null)
                 jo.put("vanity", vanity);
 
-            else if(country != null)
+            else if (country != null)
                 jo.put("country", country);
 
-            else if(city != null)
+            else if (city != null)
                 jo.put("city", city);
-            
 
-        } catch (JSONException ignored) {}
-        finally {
+        } catch (JSONException ignored) {
+        } finally {
+            try {
+                url = new URL(http);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Accept", "application/json");
 
+                OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(String.valueOf(jo));
+                writer.flush();
+                writer.close();
+                out.close();
+
+                urlConnection.connect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             // send `jo`
             // as POST body
             // to https://basic-data.parseapp.com/update
         }
     }
-
+    
     public DataUploader setLocation(String country, String city) {
         this.country = country;
         this.city = city;
