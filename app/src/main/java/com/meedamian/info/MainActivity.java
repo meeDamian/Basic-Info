b0b7eefd37184290390ded0be94ed89f87c89f9b
Rasmuhamed <a.sleszynski@gmail.com>
2015-11-19 23:41:53 +0100

    Transparent top #9

--- app/src/main/java/com/meedamian/info/MainActivity.java
+++ app/src/main/java/com/meedamian/info/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -43,66 +44,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         bd = BasicData.getInstance(this, new BasicData.DataCallback() {
             @Override
             public void onDataReady(String vanity, String phone, String country, String city) {
-            if (vanity != null)
-                vanityET.setText(vanity);
+                if (vanity != null)
+                    vanityET.setText(vanity);
 
-            if (phone != null)
-                phoneET.setText(phone);
+                if (phone != null)
+                    phoneET.setText(phone);
 
 
-            String locationQuery = null;
-            if (country != null)
-                locationQuery = country;
+                String locationQuery = null;
+                if (country != null)
+                    locationQuery = country;
 
-            if (city != null) {
-                if (locationQuery == null)
-                    locationQuery = city;
-                else
-                    locationQuery += ", " + city;
-            }
-
-            if (locationQuery != null) {
-                try {
-                    Address address = new Geocoder(MainActivity.this).getFromLocationName(locationQuery, 1).get(0);
-                    LatLng position = new LatLng(
-                        address.getLatitude(),
-                        address.getLongitude()
-                    );
-
-                    mGoogleMap.addMarker(new MarkerOptions()
-                            .position(position)
-                            .title(country + ", " + city));
-
-                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 11));
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (city != null) {
+                    if (locationQuery == null)
+                        locationQuery = city;
+                    else
+                        locationQuery += ", " + city;
                 }
-            }
+
+                if (locationQuery != null) {
+                    try {
+                        Address address = new Geocoder(MainActivity.this).getFromLocationName(locationQuery, 1).get(0);
+                        LatLng position = new LatLng(
+                                address.getLatitude(),
+                                address.getLongitude()
+                        );
+
+                        mGoogleMap.addMarker(new MarkerOptions()
+                                .position(position)
+                                .title(country + ", " + city));
+
+                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 11));
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
-        phoneET = (EditText) findViewById(R.id.phone);
-
-        TextInputLayout vanityWrapper = (TextInputLayout) findViewById(R.id.vanityWrapper);
-        vanityWrapper.setHint(String.format(
-            getString(R.string.current_url),
-            bd.getPublicId()
-        ));
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
-            .findFragmentById(R.id.map);
+                .findFragmentById(R.id.map);
 
         mGoogleMap = mapFragment.getMap();
         mGoogleMap.getUiSettings().setAllGesturesEnabled(false);
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        phoneET = (EditText) findViewById(R.id.phone);
+
+        TextInputLayout vanityWrapper = (TextInputLayout) findViewById(R.id.vanityWrapper);
+        vanityWrapper.setHint(String.format(
+                getString(R.string.current_url),
+                bd.getPublicId()
+        ));
 
         vanityET = (EditText) findViewById(R.id.vanity);
 
@@ -139,6 +144,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         init();
+    }
+
+    // A method to find height of the status bar
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
     @Override
